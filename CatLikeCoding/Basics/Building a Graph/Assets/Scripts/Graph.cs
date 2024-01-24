112dfdfc7f690a2d195f2f0ab344c9d2d533fe29
Aristotle Maule <aristotle.maule@hotmail.com>
2024-01-24 17:55:52 -0500

    Completed projcet

--- CatLikeCoding/Basics/Building a Graph/Assets/Scripts/Graph.cs
+++ CatLikeCoding/Basics/Building a Graph/Assets/Scripts/Graph.cs
@@ -6,18 +6,44 @@ public class Graph : MonoBehaviour
 {
     [SerializeField] private Transform _pointPrefab;
 
+    [SerializeField,Range(10, 100)] private int resolution = 10;
+
+    Transform[] points;
+
     private void Awake()
     {
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
         Vector3 position = Vector3.zero;
-        var scale = Vector3.one / 5f;
 
-        for (int i = 0; i < 10; i++)
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++)
         {
-            Transform point = Instantiate(_pointPrefab);
-            position.x = (i + 0.5f) / 5f - 1f;
-            position.y = position.x * position.x;
+            Transform point = points[i] = Instantiate(_pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform, false);
         }
+    }
+
+    private void Update()
+    {
+        float _time = Time.time;
+
+        for (int i = 0;i < points.Length;i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = GraphY(position, _time);
+            point.localPosition = position;
+        }
+    }
+
+    private static float GraphY(Vector3 position, float time)
+    {
+        position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+        return position.y;
     }
 }
